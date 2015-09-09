43b026f5cdd58377fbc0e9038d76c2ba560d2e13
Rannveigg13 <rannveigg13@ru.is>
2015-09-09 22:17:43 +0000

    mah

--- app/src/main/java/com/example/hrafnkell/dots/TimedActivity.java
+++ app/src/main/java/com/example/hrafnkell/dots/TimedActivity.java
@@ -1,11 +1,12 @@
 package com.example.hrafnkell.dots;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class TimedActivity extends AppCompatActivity {
+public class TimedActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
